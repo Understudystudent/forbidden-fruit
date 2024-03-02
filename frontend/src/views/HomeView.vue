9a546d4371a424107df1460b9ac97c1ac0ee756c
Carl James <carlmjames2@gmail.com>
2024-03-02 12:11:59 -0500

    made the firstPart of the landing Page

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <BannerHome/>
+    <CategoryProducts
+      :tabs="womentabs"
+      :title="womentitle"
+      :products="womenProducts"
+    />
+  </div>
+</template>
+
+<script setup>
+import BannerHome from "../components/BannerHome.vue";
+import CategoryProducts from "../components/CategoryProducts.vue";
+
+</script>
