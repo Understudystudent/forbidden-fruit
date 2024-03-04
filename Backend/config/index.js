498c015815ebd559b3a8d6928cc0404d5742dcc2
Understudystudent <carlmjames2@gmail.com>
2024-03-04 15:08:06 +0200

    connected backend to database

--- Backend/config/index.js
+++ Backend/config/index.js
@@ -1,1 +1,25 @@
-//
+import { createPool } from "mysql";
+import { config } from "dotenv";
+
+config();
+
+let connection;
+
+try {
+    connection = createPool({
+        host: process.env.DB_HOST,
+        database: process.env.DB_Name,
+        user: process.env.DB_UserName,
+        password: process.env.DB_userPass,
+        multipleStatement: true,
+        connectionLimit: 30,
+    });
+} 
+catch
+ (error) {
+    console.error("Error creating database connection:", error);
+}
+
+export {
+     connection
+     };
