4e75a96a6ddaa34b018277282d8e26c68ebb60ab
Understudystudent <carlmjames2@gmail.com>
2024-03-07 14:10:55 +0200

    updated login

--- frontend/src/Service/AuthenticateUser.js
+++ frontend/src/Service/AuthenticateUser.js
@@ -1,26 +1,44 @@
-// import axios from "axios";
-// function applyToken(token) {
-//     if(token) {
-//         axios.defaults.headers = {
-//             Authorization: `${token}`
-//         }
-//     }
-// }
-
-// second option
 import axios from "axios";
 
 function applyToken(token) {
-    if (token) {
-        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    if(token) {
+        axios.defaults.headers = {
+            Authorization: `${token}`
+        }
     }
 }
 
-function clearToken() {
-    axios.defaults.headers.common['Authorization'] = undefined;
-}
+// second option
+// import axios from "axios";
+
+// function applyToken(token) {
+//     if (token) {
+//         axios.defaults.headers.common['Authorization'] = ` ${token}`;
+//     }
+// }
+
+// function clearToken() {
+//     axios.defaults.headers.common['Authorization'] = undefined;
+// }
+
+
+// import axios from 'axios';
+// import { useCookies } from 'vue3-cookies';
+
+// const { cookies } = useCookies();
+
+// function applyToken(token) {
+//   if (token) {
+//     axios.defaults.headers = {
+//       Authorization: `${token}`
+//     };
+//     cookies.set('authToken', token, { expires: 10 }); 
+//   }
+// }
+
+export { applyToken };
 
 export default {
     applyToken,
-    clearToken
+    // clearToken
 };
