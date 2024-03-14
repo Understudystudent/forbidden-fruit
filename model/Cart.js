7eeb760881bc3654cc06685a4ef30bdb294de248
Understudystudent <carlmjames2@gmail.com>
2024-03-14 16:18:45 +0200

    hope the cart is working will have to check third push

---
+++ model/Cart.js
@@ -0,0 +1,73 @@
+import { connection as db } from "../config/index.js";
+import { verifyAToken } from "../middleware/AuthenticateUser.js";
+
+class Cart {
+    // Fetch cart items for a user
+    getCartItems(req, res) {
+        const userID = req.params.userID;
+        const qry = `
+            SELECT *
+            FROM Cart
+            WHERE userID = ?
+        `;
+        db.query(qry, userID, (err, results) => {
+            if (err) throw err;
+            res.json({
+                status: res.statusCode,
+                results
+            });
+        });
+    }
+
+    // Add item to cart
+    addItem(req, res) {
+        const { userID, itemID, quantity } = req.body;
+        const qry = `
+            INSERT INTO Cart (userID, itemID, quantity)
+            VALUES (?, ?, ?)
+        `;
+        db.query(qry, [userID, itemID, quantity], (err) => {
+            if (err) throw err;
+            res.json({
+                status: res.statusCode,
+                msg: 'Item added to cart successfully.'
+            });
+        });
+    }
+
+    // Update cart item quantity
+    updateCartItem(req, res) {
+        const { userID, itemID } = req.params;
+        const { quantity } = req.body;
+        const qry = `
+            UPDATE Cart
+            SET quantity = ?
+            WHERE userID = ? AND itemID = ?
+        `;
+        db.query(qry, [quantity, userID, itemID], (err) => {
+            if (err) throw err;
+            res.json({
+                status: res.statusCode,
+                msg: 'Cart item quantity updated successfully.'
+            });
+        });
+    }
+
+    // Remove item from cart
+    removeCartItem(req, res) {
+        const { userID, itemID } = req.params;
+        const qry = `
+            DELETE FROM Cart
+            WHERE userID = ? AND itemID = ?
+        `;
+        db.query(qry, [userID, itemID], (err) => {
+            if (err) throw err;
+            res.json({
+                status: res.statusCode,
+                msg: 'Item removed from cart successfully.'
+            });
+        });
+    }
+}
+
+export { Cart };
--- model/index.js
+++ model/index.js
@@ -1,11 +1,14 @@
 import { Users } from "./Users.js";
 import { Items } from "./Item.js";
+import {Cart} from "./Cart.js";
 
 // Create objects
 let users = new Users()
 let items = new Items()
+let cart = new Cart();
 
 export{
     users, 
-    items
+    items,
+    cart
 }
