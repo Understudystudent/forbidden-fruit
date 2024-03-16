e2a741db166f6e3cec8514c5cb8143f66073ea9f
Carl James <carlmjames2@gmail.com>
2024-03-16 04:17:57 -0400

    made a basic Spinner

--- frontend/src/components/SpinnerComponent.vue
+++ frontend/src/components/SpinnerComponent.vue
@@ -1,15 +1,12 @@
-<!-- Spinner.vue -->
 <template>
     <div class="spinner-container" v-if="loading">
-      <div class="spinner">
-        <img src="../assets/Screenshot-2024-03-09-8-29-56-PM-removebg-preview.png" alt="Center Image" class="center-image" loading="lazy"/>
-      </div>
+      <div class="spinner"></div>
     </div>
   </template>
   
   <script>
   export default {
-    name: 'Spinner',
+    name: 'SpinnerLoader',
     props: {
       loading: {
         type: Boolean,
@@ -21,24 +18,24 @@
   
   <style scoped>
   .spinner-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.5); 
   }
   
   .spinner {
-    position: relative;
-    width: 50px;
-    height: 50px;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    border: 4px solid #fff; 
+    border-top: 4px solid transparent; 
     animation: spin 1s linear infinite;
-  }
-  
-  .center-image {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
   }
   
   @keyframes spin {
