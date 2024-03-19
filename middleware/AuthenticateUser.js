190febd7cb3dcd76d3efebeac211030c86f49946
Understudystudent <carlmjames2@gmail.com>
2024-03-19 11:10:41 +0200

    change toke

--- middleware/AuthenticateUser.js
+++ middleware/AuthenticateUser.js
@@ -14,42 +14,42 @@ function createToken(user){
     )
 }
 
-function verifyAToken(req, res, next) {
-    const token = req.cookies.userAuthenticated;  // cookie name is 'userAuthenticated'
-    console.log(token);
-    if (token) {
-        jwt.verify(token, process.env.SECRET_KEY, (err) => {
-            if (err) {
-                res.status(401).json({ error: 'Unauthorized' });
-            } else {
-                // Token is valid, continue with the request
-                next();
-            }
-        });
-    } else {
-        res.status(401).json({ error: 'Please login.' });
-    }
-}
-
-// Retrieve a token from the browser
 // function verifyAToken(req, res, next) {
-//     const token = req?.headers['Authorization']
-//     if(token) {
-//         if(verify(token, process.env.SECRET_KEY)){
-//             next()
-//         }else {
-//             res?.json({
-//                 status: res.statusCode,
-//                 msg: "Please provide the correct credentials."
-//             })
-//         }
-//     }else {
-//         res?.json({
-//             status: res.statusCode,
-//             msg: "Please login."
-//         })
+//     const token = req.cookies.userAuthenticated;  // cookie name is 'userAuthenticated'
+//     console.log(token);
+//     if (token) {
+//         jwt.verify(token, process.env.SECRET_KEY, (err) => {
+//             if (err) {
+//                 res.status(401).json({ error: 'Unauthorized' });
+//             } else {
+//                 // Token is valid, continue with the request
+//                 next();
+//             }
+//         });
+//     } else {
+//         res.status(401).json({ error: 'Please login.' });
 //     }
 // }
+
+// Retrieve a token from the browser
+function verifyAToken(req, res, next) {
+    const token = req?.headers['Authorization']
+    if(token) {
+        if(verify(token, process.env.SECRET_KEY)){
+            next()
+        }else {
+            res?.json({
+                status: res.statusCode,
+                msg: "Please provide the correct credentials."
+            })
+        }
+    }else {
+        res?.json({
+            status: res.statusCode,
+            msg: "Please login."
+        })
+    }
+}
 
 
 export {
