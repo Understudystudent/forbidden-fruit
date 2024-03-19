bac0d08095b25a0dd11fed0542e0e3361575e384
Understudystudent <carlmjames2@gmail.com>
2024-03-19 14:02:20 +0200

    big backend updat might need to revert part 3 focus on Users.js

--- model/Users.js
+++ model/Users.js
@@ -1,41 +1,44 @@
-import {
-    connection as db
-} from "../config/index.js";
-import {
-    hash,
-    compare
-} from 'bcrypt';
-import {
-    createToken
-} from "../middleware/AuthenticateUser.js";
+import { connection as db } from "../config/index.js";
+import { hash, compare } from 'bcrypt';
+import { createToken } from "../middleware/AuthenticateUser.js";
 
 class Users {
     // Fetch all users
     fetchUsers(req, res) {
         const qry = `
-            SELECT userID, firstName, lastName,
-            userAge, Gender, userRole, emailAdd,userProfile,userImg,address,number
+            SELECT userID, firstName, lastName, 
+            userAge, Gender, emailAdd, userPwd, userRole,
+            userProfile, userImg, address, number
             FROM Users;
         `;
         db.query(qry, (err, results) => {
-            if (err) throw err;
-            res.json({
-                status: res.statusCode,
-                results
-            });
+            if (err) {
+                console.error('Error fetching users:', err);
+                res.status(500).json({
+                    status: 500,
+                    error: 'Internal Server Error'
+                });
+            } else {
+                res.json({
+                    status: res.statusCode,
+                    results
+                });
+            }
         });
     }
 
     // Fetch a single user
     fetchUser(req, res) {
         const qry = `
-            SELECT userID, firstName, lastName,
-            userAge, Gender, userRole, emailAdd, userProfile, userImg, address, number
+            SELECT userID, firstName, lastName, 
+            userAge, Gender, emailAdd, userPwd, userRole,
+            userProfile, userImg, address, number
             FROM Users
-            WHERE userID = ${req.params.id};`; 
-        
-        db.query(qry, [userID], (err, result) => {
-            if (err) {
+            WHERE userID = ${req.params.id};
+            `;
+
+            db.query(qry, (err, result)=>{
+                if (err) {
                 console.error('Error fetching user:', err);
                 res.status(500).json({
                     status: 500,
@@ -44,7 +47,7 @@ class Users {
             } else {
                 res.json({
                     status: res.statusCode,
-                    result: result[0] 
+                    result: result[0]
                 });
             }
         });
@@ -53,165 +56,131 @@ class Users {
     // Create a new user
     async createUser(req, res) {
         let data = req.body;
-        data.userPwd = await hash(data.userPwd, 9);
-
-        let user = {
-            emailAdd: data.emailAdd,
-            userPwd: data.userPwd
-        };
-
-        const qry = `INSERT INTO Users SET ?;`;
-
-        db.query(qry, [data], (err) => {
-            if (err) {
-                res.json({
-                    status: res.statusCode,
-                    msg: err
-                });
-            } else {
-                let token = createToken(user);
-                res.json({
-                    status: res.statusCode,
-                    token,
-                    msg: 'You\'re registered'
-                });
-            }
-        });
-    }
-
-    // Update user information
-    async updateUser(req, res) {
-        const {
-            id
-        } = req.params;
-        const userData = req.body;
         try {
-            if (userData && userData.userPwd) {
-                userData.userPwd = await hash(userData.userPwd, 9);
-            }
-
-            const qry = `
-            UPDATE Users
-            SET ?
-            WHERE userID = ?
-        `;
-
-            db.query(qry, [userData, id], (err) => {
-                if (err) throw err;
-                res.json({
-                    status: res.statusCode,
-                    msg: "The user information is updated."
-                });
+            data.userPwd = await hash(data.userPwd, 9);
+            const qry = `INSERT INTO Users SET ?`;
+            db.query(qry, [data], (err) => {
+                if (err) {
+                    console.error('Error creating user:', err);
+                    res.status(500).json({
+                        status: 500,
+                        error: 'Internal Server Error'
+                    });
+                } else {
+                    let user = {
+                        emailAdd: data.emailAdd,
+                        userPwd: data.userPwd
+                    };
+                    let token = createToken(user);
+                    res.json({
+                        status: res.statusCode,
+                        token,
+                        msg: 'You\'re registered'
+                    });
+                }
             });
         } catch (error) {
-            console.error("Error updating user:", error);
+            console.error('Error creating user:', error);
             res.status(500).json({
                 status: 500,
-                msg: "An error occurred when updating the user information."
+                error: 'Internal Server Error'
             });
         }
     }
 
+    // Update user information
+    async updateUser(req, res) {
+        const data = req.body;
+        try {
+            if (data.userPwd) {
+                data.userPwd = await hash(data.userPwd, 8);
+            }
+            const qry = `UPDATE Users SET ? WHERE userID = ?`;
+            db.query(qry, [data, req.params.id], (err) => {
+                if (err) {
+                    console.error('Error updating user:', err);
+                    res.status(500).json({
+                        status: 500,
+                        error: 'Internal Server Error'
+                    });
+                } else {
+                    res.json({
+                        status: res.statusCode,
+                        msg: 'The user information is updated.'
+                    });
+                }
+            });
+        } catch (error) {
+            console.error('Error updating user:', error);
+            res.status(500).json({
+                status: 500,
+                error: 'Internal Server Error'
+            });
+        }
+    }
 
     // Delete a user
     deleteUser(req, res) {
-        const qry = `
-            DELETE FROM Users
-            WHERE userID = ${req.params.id};
-        `;
-        db.query(qry, (err) => {
-            if (err) throw err;
-            res.json({
-                status: res.statusCode,
-                msg: "The user information has been removed."
-            });
-        });
-    }
-
-    // Delete user's own profile
-    deleteUserProfile(req, res) {
-        const userId = req.params.id;
-        const qry = `
-            DELETE FROM Users
-            WHERE userID = ${userId};
-        `;
-        db.query(qry, (err) => {
+        const qry = `DELETE FROM Users WHERE userID = ?`;
+        db.query(qry, [req.params.id], (err) => {
             if (err) {
-                console.error("Error deleting user profile:", err);
+                console.error('Error deleting user:', err);
                 res.status(500).json({
                     status: 500,
-                    msg: "An error occurred when deleting the user profile."
+                    error: 'Internal Server Error'
                 });
             } else {
                 res.json({
                     status: res.statusCode,
-                    msg: "Your profile has been deleted."
+                    msg: 'The user information has been removed.'
                 });
             }
         });
     }
 
     // User login
-    async login(req, res) {
-        const {
-            emailAdd,
-            userPwd
-        } = req.body;
-
-        try {
-            const qry = `
+    login(req, res) {
+        const { emailAdd, userPwd } = req.body;
+        const qry = `
             SELECT userID, firstName, lastName, 
-            userAge, Gender, emailAdd, userPwd, userRole
+            userAge, Gender, emailAdd, userPwd, userRole,
+            userProfile, userImg, address, number
             FROM Users
             WHERE emailAdd = ?;
         `;
-            db.query(qry, [emailAdd], async (err, result) => {
-                if (err) throw err;
-
-                if (!result || result.length === 0) {
+        db.query(qry, [emailAdd], async (err, result) => {
+            if (err) {
+                console.error('Error logging in:', err);
+                res.status(500).json({
+                    status: 500,
+                    error: 'Internal Server Error'
+                });
+            } else {
+                if (!result.length) {
                     res.json({
                         status: res.statusCode,
-                        msg: "You provided a wrong email address."
+                        msg: 'You provided a wrong email address.'
                     });
                 } else {
                     const validPass = await compare(userPwd, result[0].userPwd);
                     if (validPass) {
-                        const token = createToken({
-                            emailAdd,
-                            userPwd
-                        });
-
-                        // Set token as a cookie
-                        res.cookie('token', token, {
-                            httpOnly: true
-                        });
-
+                        const token = createToken({ emailAdd, userPwd });
                         res.json({
                             status: res.statusCode,
-                            msg: "You're logged in",
+                            msg: 'You\'re logged in',
                             token,
                             result: result[0]
                         });
                     } else {
                         res.json({
                             status: res.statusCode,
-                            msg: "Please provide the correct password."
+                            msg: 'Please provide the correct password.'
                         });
                     }
                 }
-            });
-        } catch (error) {
-            console.error("Error during login:", error);
-            res.status(500).json({
-                status: 500,
-                msg: "An error occurred during login."
-            });
-        }
+            }
+        });
     }
-
-
 }
 
-export {
-    Users
-};
+export { Users };
