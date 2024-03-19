a3c20ef3571dc91d43225a092402af24d4d226fe
Understudystudent <carlmjames2@gmail.com>
2024-03-19 11:29:44 +0200

    updates

--- middleware/AuthenticateUser.js
+++ middleware/AuthenticateUser.js
@@ -33,7 +33,7 @@ function createToken(user){
 
 // Retrieve a token from the browser
 function verifyAToken(req, res, next) {
-    const token = req?.headers['authorization']
+    const token = req?.headers["authorization"]
     console.log(token);
     if(token) {
         if(verify(token, process.env.SECRET_KEY)){
